3bfa68e01692ec4e8b3d807a89bbbbfe1fa9f0e6
Evgheni Velcev <velcev.e@avantajprim.com>
2026-02-20 15:27:02 +0200

    https://redmine.dev-a1.org/issues/28547 02. Изучение doctrine add tests

---
+++ tests/Entity/AuthorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Author;
+use PHPUnit\Framework\TestCase;
+
+class AuthorTest extends TestCase
+{
+    private Author $author;
+
+    protected function setUp(): void
+    {
+        $this->author = new Author();
+    }
+
+    public function testGetIdReturnsNull(): void
+    {
+        $this->assertNull($this->author->getId());
+    }
+
+    public function testSetAndGetName(): void
+    {
+        $name = 'F. Scott Fitzgerald';
+        $this->author->setName($name);
+
+        $this->assertEquals($name, $this->author->getName());
+    }
+
+    public function testSetNameReturnsSelf(): void
+    {
+        $result = $this->author->setName('Test Author');
+
+        $this->assertSame($this->author, $result);
+    }
+}
---
+++ tests/Entity/BookTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Book;
+use PHPUnit\Framework\TestCase;
+
+class BookTest extends TestCase
+{
+    private Book $book;
+
+    protected function setUp(): void
+    {
+        $this->book = new Book();
+    }
+
+    public function testGetIdReturnsNull(): void
+    {
+        $this->assertNull($this->book->getId());
+    }
+
+    public function testSetAndGetName(): void
+    {
+        $name = 'The Great Gatsby';
+        $this->book->setName($name);
+
+        $this->assertEquals($name, $this->book->getName());
+    }
+
+    public function testSetNameReturnsSelf(): void
+    {
+        $result = $this->book->setName('Test Book');
+
+        $this->assertSame($this->book, $result);
+    }
+}
