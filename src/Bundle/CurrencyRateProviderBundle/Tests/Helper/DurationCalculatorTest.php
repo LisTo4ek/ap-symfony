42c7823fcc02d47ffc6534e3f6e359073c91efa0
Evgheni Velcev <velcev.e@avantajprim.com>
2026-03-04 11:35:35 +0200

    - refactor

--- src/Bundle/CurrencyRateProviderBundle/Tests/Helper/DurationCalculatorTest.php
+++ src/Bundle/CurrencyRateProviderBundle/Tests/Helper/DurationCalculatorTest.php
@@ -73,7 +73,7 @@ class DurationCalculatorTest extends TestCase
      */
     public function testMeasureWithCallableValue(): void
     {
-        $callable =static fn() => 42;
+        $callable = static fn() => 42;
 
         $result = DurationCalculator::measure($callable);
 
